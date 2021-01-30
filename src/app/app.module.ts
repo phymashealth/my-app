9c6f4f28486cc8f943aab9a4093df2c5b0213492
Dawson Li <dawson.on@gmail.com>
2021-01-30 01:00:30 +0000

    app.module.ts edited online with Bitbucket

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationModule } from '@angular/platform-browser/animations';
+import { MatTollbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationModule,
+    MatTollbarModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
