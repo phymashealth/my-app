321f50d9b21a6dfd252d6b17067e679ce91d79da
phymashealth <phymas.health@gmail.com>
2021-01-31 09:42:04 -0500

    2 files modified, 1 added

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,10 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Dish} from '../share/dish';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
+const DISHES:Dish[]=[
+  
+  { id:0,
+    name:'pizza',
+    image:'../asset/image/pizza',
+    category:'large',
+    featured:true,
+    price:'$12.99',
+    description:'3 toopings'
+  },
+  { id:1,
+    name:'donut',
+    image:'../asset/image/donut',
+    category:'double',
+    featured:false,
+    price:'$5.99',
+    description:'glazed'
+  },
+];
 export class MenuComponent implements OnInit {
 
   constructor() { }
